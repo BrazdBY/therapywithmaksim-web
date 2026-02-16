0bd16da14c956a3c4a9516363a604c6464a8b614
Maksim Pahuliayeu <brazd.by@gmail.com>
2026-02-16 12:39:35 +0400

    Updates

--- assets/About-29MVG3eD.js
+++ assets/About-29MVG3eD.js
@@ -1,4 +1,4 @@
-import{c as t,j as e,S as r,I as s,C as o}from"./index-BG5EeTix.js";import{Z as n}from"./zap-CGGRiqP-.js";/**
+import{c as t,j as e,S as r,I as s,C as o}from"./index-CBNmy6QE.js";import{Z as n}from"./zap-B1xZkYQO.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/Home-DnbzKBsV.js
+++ assets/Home-DnbzKBsV.js
@@ -1,4 +1,4 @@
-import{c as n,j as e,m as i,B as g,F as f,L as d,I as a,S as r,A as h,T as b,a as N,C as u}from"./index-BG5EeTix.js";import{C as t}from"./circle-check-BrsfOSEd.js";import{Z as v}from"./zap-CGGRiqP-.js";/**
+import{c as n,j as e,m as i,B as g,F as f,L as d,I as a,S as r,A as h,T as b,a as N,C as u}from"./index-CBNmy6QE.js";import{C as t}from"./circle-check-3YAXc-Ai.js";import{Z as v}from"./zap-B1xZkYQO.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
